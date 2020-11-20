db8cc922df61d28de471e39b3eb291bb6c2eccff
Leon Teubert <Leon.Teubert@rohde-schwarz.com>
2020-11-20 22:34:05 +0100

    add handler

---
+++ seatfinder_rest/src/main/java/com/rohdeschwarz/hackathon20/seatfinder_rest/handler/RecommendationHandler.java
@@ -0,0 +1,13 @@
+package com.rohdeschwarz.hackathon20.seatfinder_rest.handler;
+
+import com.rohdeschwarz.hackathon20.seatfinder_rest.model.Recommendation;
+import com.rohdeschwarz.hackathon20.seatfinder_rest.model.RecommendationRequest;
+
+public class RecommendationHandler {
+
+  public Recommendation calculateRecommendation(RecommendationRequest request){
+   // TODO implement
+    throw new RuntimeException("not implemented");
+  }
+
+}
