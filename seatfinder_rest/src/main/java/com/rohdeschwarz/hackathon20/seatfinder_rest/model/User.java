018c39e2e5bd555bf9c23060c095b142ef4a1e2f
fbau-rs <>
2020-11-20 23:08:25 +0100

    File storage for Schedule and Map added

--- seatfinder_rest/src/main/java/com/rohdeschwarz/hackathon20/seatfinder_rest/model/User.java
+++ seatfinder_rest/src/main/java/com/rohdeschwarz/hackathon20/seatfinder_rest/model/User.java
@@ -3,6 +3,7 @@ package com.rohdeschwarz.hackathon20.seatfinder_rest.model;
 public class User {
   public int id;
   public String name;
+  public String team;
 
   public User() {
   }
@@ -10,5 +11,6 @@ public class User {
   public User(int id, String name) {
     this.id = id;
     this.name = name;
+    this.team = team;
   }
 }
