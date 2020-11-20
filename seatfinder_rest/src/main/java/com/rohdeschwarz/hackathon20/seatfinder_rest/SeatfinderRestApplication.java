b4ddb1f4af1846b1f7167836ceb2d0e47c2d39be
Leon Teubert <Leon.Teubert@rohde-schwarz.com>
2020-11-20 16:07:32 +0100

    add backend project

---
+++ seatfinder_rest/src/main/java/com/rohdeschwarz/hackathon20/seatfinder_rest/SeatfinderRestApplication.java
@@ -0,0 +1,13 @@
+package com.rohdeschwarz.hackathon20.seatfinder_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatfinderRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeatfinderRestApplication.class, args);
+    }
+
+}
---
+++ seatfinder_rest/src/test/java/com/rohdeschwarz/hackathon20/seatfinder_rest/SeatfinderRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohdeschwarz.hackathon20.seatfinder_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatfinderRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
